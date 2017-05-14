65b3c1a529228c5f79c39c64ea8cdf489977f3bc
Renan Bastos <renanbastos.tec@gmail.com>
2017-05-14 01:51:26 -0400

    [*] added promises and libs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const app = require('upfiles')()
-			.include('config').exec();
-const server = app.express;
-
-server.listen(3000, () => console.log("Server this is open! localhost: 3000"));
-
+'use strict';
+Promise = global.Promise || Promise;
+require('loaded-module');
+loadedModule('utils/connection');
+loadedModule('routes');
