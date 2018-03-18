35f5d672726c28ede1981a59847292d535c824de
renanbastos93 <renanbastos.tec@gmail.com>
2018-03-17 22:57:29 -0300

    added log of starting application

---
+++ bin/server.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const app = require("../src/app");
+const http = require("http");
+const debug = require("debug")("nodestr:server");
+const express = require("express");
+const config = require("config");
+
+const port = config.get("server.port");
+app.set("ip", config.get("server.ip"));
+app.set("port", port);
+
+const server = http.createServer(app);
+
+function onError(error) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    const bind = typeof port === "string" ?
+        "Pipe " + port :
+        "Port " + port;
+
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string"
+    ? "pipe " + addr
+    : "port " + addr.port;
+  debug("Listening on " + bind);
+};
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+console.log(`Server stater in PORT=${port} PID=${process.pid}`);
