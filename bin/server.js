11b042e4d61b0335a63dc5009f76e36b6cf2cc6e
renanbastos93 <renanbastos.tec@gmail.com>
2018-03-20 03:43:30 -0300

    changed to test deployment in herokuapp

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const app = require("../src/app");
-const http = require("http");
 const debug = require("debug")("nodestr:server");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -13,41 +12,4 @@ app.set("port", port);
 
 mongoose.connect(config.get("mongodb.queryString"));
 
-const server = http.createServer(app);
-
-function onError(error) {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-
-    const bind = typeof port === "string" ?
-        "Pipe " + port :
-        "Port " + port;
-
-    switch (error.code) {
-        case "EACCES":
-            console.log(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.log(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string"
-    ? "pipe " + addr
-    : "port " + addr.port;
-  debug("Listening on " + bind);
-};
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-console.log(`Server stater in PORT=${port} PID=${process.pid}`);
+app.listen(app.get("port"), console.log(`Server stater in PORT=${port} PID=${process.pid}`));
--- src/middlewares/generic-middleware.js
+++ src/middlewares/generic-middleware.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const middleware = (request, response, next) => {
-    request.header("Access-Control-Allow-Origin", "*");
+    request.header("Access-Control-Allow-Origin", "http://localhost:4200/");
     request.header("Access-Control-Allow-Methods", ["GET", "POST", "PUT", "OPTIONS"]);
     request.header(
         "Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Origin, X-Requested-With, Content-Type, Authorization, Accept, Access-Control-Request-Method, Access-Control-Request-Headers"
     );
     return next();
 };
