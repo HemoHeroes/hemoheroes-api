6c860807f37a9dc3c9eba52eb54eb9d091157ad7
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-03-20 03:36:05 -0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const app = require("../src/app");
-const http = require("http");
 const debug = require("debug")("nodestr:server");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -13,41 +12,7 @@ app.set("port", process.env.PORT || 8080);
 
 mongoose.connect(config.get("mongodb.queryString"));
 
-const server = http.createServer(app);
+app.listen(port, ()=>{
+    console.log(`Server stater in PORT=${port} PID=${process.pid}`);
+});
 
-function onError(error) {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-
-    const bind = typeof port === "string" ?
-        "Pipe " + port :
-        "Port " + port;
-
-    switch (error.code) {
-        case "EACCES":
-            console.log(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.log(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string"
-    ? "pipe " + addr
-    : "port " + addr.port;
-  debug("Listening on " + bind);
-};
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-console.log(`Server stater in PORT=${port} PID=${process.pid}`);
