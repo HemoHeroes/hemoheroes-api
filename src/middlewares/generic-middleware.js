b5a5124675c2c104c9d3b30dcb9640bfc9df534d
renanbastos93 <renanbastos.tec@gmail.com>
2018-04-10 03:02:38 -0300

    accept method post on cors

--- src/middlewares/generic-middleware.js
+++ src/middlewares/generic-middleware.js
@@ -4,7 +4,7 @@ const middleware = (request, response, next) => {
     response.append("Cache-Control", "public, max-age=120");
     response.append("Access-Control-Allow-Origin", "*");
     response.append("Access-Control-Allow-Credentials", true)
-    response.append("Access-Control-Allow-Methods", ["GET", "OPTIONS"]);
+    response.append("Access-Control-Allow-Methods", ["GET", "POST", "OPTIONS"]);
     response.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
     return next();
 };
